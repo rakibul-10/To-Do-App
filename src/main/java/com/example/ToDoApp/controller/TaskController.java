64fde2185eed99a7df349c5325ce9528c3154fd4
Rakibul Islam <rakibulcste15@gmail.com>
2025-02-12 23:37:12 +0600

    Initial commit

---
+++ src/main/java/com/example/ToDoApp/controller/TaskController.java
@@ -0,0 +1,43 @@
+package com.example.ToDoApp.controller;
+
+import com.example.ToDoApp.Services.TaskService;
+import com.example.ToDoApp.model.Task;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+//@RequestMapping("/tasks")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public String getTasks(Model model) {
+        List<Task> tasks = taskService.getAllTasks();
+        model.addAttribute("tasks", tasks);
+        return "tasks";
+    }
+
+    @PostMapping
+    public String createTasks(@RequestParam String title) {
+        taskService.createTasks(title);
+        return "redirect:/";
+    }
+    @GetMapping("{id}/delete")
+    public String deleteTasks(@PathVariable Long id) {
+        taskService.deleteTasks(id);
+        return "redirect:/";
+    }
+    @GetMapping("{id}/toggle")
+    public String toggleTasks(@PathVariable Long id) {
+        taskService.toggleTasks(id);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/example/ToDoApp/repository/TaskRepository.java
@@ -0,0 +1,8 @@
+package com.example.ToDoApp.repository;
+
+import com.example.ToDoApp.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task,Long> {
+
+}
